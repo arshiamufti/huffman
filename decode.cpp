6db2df3ec2df1dbc6da6b160b45cd9cfe3b61616
Arshia <arshia@Arshias-MacBook-Pro.local>
2016-02-08 00:35:43 -0500

    naive decoding
    naive decode

---
+++ decode.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+// TODO: make `decode` more efficient using memoization maybe?
+// TODO: write `decode` using recursion
+// TODO: read up on improvements to huffman coding
+// TODO: write better tests
+// TODO: write `encode`
+
+struct node {
+    int freq;
+    char data;
+    node * left;
+    node * right;
+};
+
+bool is_leaf(node * n) {
+    return !(n->left || n->right);
+}
+
+string decode_huff(node * root,string s) {
+    if (!root) return "";
+    node * n = root;
+    string res = "";
+    for (int i = 0; i < s.length(); ++i) {
+        if (s[i] == '0') {
+            n = n->left;
+        } else if (s[i] == '1') {
+            n = n->right;
+        }
+        
+        if (is_leaf(n)) {
+            res += n->data;
+            n = root;
+        }
+    }
+    cout << res;
+    return res;
+}
+
+
